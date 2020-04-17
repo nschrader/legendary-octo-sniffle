699af76b4048fec0b8a60b047ee663f5788f9238
joaoolival <joaoljolival@gmail.com>
2020-04-17 19:23:05 +0100

    packages and classes

--- src/main/java/legendary/octo/sniffle/App.java
+++ src/main/java/legendary/octo/sniffle/App.java
@@ -5,7 +5,7 @@ package legendary.octo.sniffle;
 
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "Hello world Test";
     }
 
     public static void main(String[] args) {
