afabf6e4a3cfb2bf1b9062a659d9e2792c748f71
Nick Schrader <nick.schrader@mailbox.org>
2020-05-02 00:59:07 -0300

    Refactored BmpFileTest

--- src/test/java/legendary/octo/sniffle/io/BmpFileTest.java
+++ src/test/java/legendary/octo/sniffle/io/BmpFileTest.java
@@ -11,6 +11,7 @@ import com.google.common.io.Resources;
 import org.junit.jupiter.api.Test;
 
 import lombok.NonNull;
+import lombok.SneakyThrows;
 
 public class BmpFileTest {
     private @NonNull byte[] load(@NonNull String path) {
@@ -22,8 +23,9 @@ public class BmpFileTest {
     }
 
     @Test
+    @SneakyThrows
     public void testOk() {
-        var file = new BmpFile(load("ok.bmp"));
+        var file = new BmpFile(Resources.toByteArray(Resources.getResource("ok.bmp")));
         assertEquals(4, file.getWidth());
         assertEquals(5, file.getHeight());
         assertEquals(2835, file.getHorizontalResolution());
@@ -31,17 +33,19 @@ public class BmpFileTest {
     }
 
     @Test
+    @SneakyThrows
     public void testJpeg() {
         assertTrue(
-            assertThrows(BmpFileException.class, () -> new BmpFile(load("jpeg.bmp")))
+            assertThrows(BmpFileException.class, () -> new BmpFile(Resources.toByteArray(Resources.getResource("jpeg.bmp"))))
             .getMessage()
             .contains("COMPRESSION"));
     }
 
     @Test
+    @SneakyThrows
     public void test32b() {
         assertTrue(
-            assertThrows(BmpFileException.class, () -> new BmpFile(load("32b.bmp")))
+            assertThrows(BmpFileException.class, () -> new BmpFile(Resources.toByteArray(Resources.getResource("32b.bmp"))))
             .getMessage()
             .contains("DEPTH"));
     }
@@ -52,8 +56,9 @@ public class BmpFileTest {
     }
 
     @Test
+    @SneakyThrows
     public void testImageData() {
-        var file = new BmpFile(load("ok.bmp"));
+        var file = new BmpFile(Resources.toByteArray(Resources.getResource("ok.bmp")));
         assertEquals(0x13, file.getImageData(0));
         assertEquals(0x07, file.getImageData(4*5*3-1));
     }
